2198d8f50fcc76f6702a89351950074c3d5395a4
rafaels549 <rafalesantoscostat@gmail.com>
2024-10-30 10:33:38 -0300

    .

--- Bnode.java
+++ Bnode.java
@@ -2,12 +2,15 @@ public class Bnode {
     private int x;
     private Bnode esq, dir;
     int critical = 0;
+    private int altura;
+   
    
 
     public Bnode(int valor) {
         x = valor;
         esq = null;
         dir = null;
+        altura = 1 ;
     }
 
     public void add(int valor) {
@@ -26,7 +29,7 @@ public class Bnode {
 
         int fatorBalanceamento = calcularFatorDeBalanceamento(this);
 
-        if (calcularFatorDeBalanceamento(this) == -2) {
+        if (calcularFatorDeBalanceamento(this) == -2 || calcularFatorDeBalanceamento(this) == 2 ) {
             System.out.println("Fator de balanceamento do nó " + valor + ": " + 0);
            
         }
@@ -34,14 +37,43 @@ public class Bnode {
       
         System.out.println("Fator de balanceamento do nó " + x + ": " + fatorBalanceamento);
       
-        if(fatorBalanceamento == -2){
+        if(fatorBalanceamento == -2 || fatorBalanceamento == 2 ){
             System.out.println("Nó crítico: "+ x);
-            System.out.println("Rotação a esquerda ...");
-              this.rotacaoEsquerda(this);
+            System.out.println("Balanceando ...");
+            atualizarAltura(this);
+            balancear(this);
         }
     }
   
-    
+    public void atualizarAltura(Bnode no) {
+        int alturaEsq = 0, alturaDir = 0;
+        if (no.esq != null) alturaEsq = no.esq.altura;
+        if (no.dir != null) alturaDir = no.dir.altura;
+        no.altura = Math.max(alturaEsq, alturaDir) + 1;
+    }
+    public Bnode balancear(Bnode no) {
+        int fatorBalanceamento = calcularFatorDeBalanceamento(no);
+
+       
+        if (fatorBalanceamento < -1) {
+            if (calcularFatorDeBalanceamento(no.dir) > 0) {
+                System.out.println("Rotação a direita ...");
+                no.dir = rotacaoDireita(no.dir);
+            }
+            System.out.println("Rotação a esquerda ...");
+            return rotacaoEsquerda(no); 
+        }
+        if (fatorBalanceamento > 1) {
+            if (calcularFatorDeBalanceamento(no.esq) < 0) {
+                System.out.println("Rotação a esquerda ...");
+                no.esq = rotacaoEsquerda(no.esq); 
+            }
+            System.out.println("Rotação a direita ...");
+            return rotacaoDireita(no); 
+        }
+
+        return no; 
+    }
     public int calcularAltura(Bnode node) {
         if (node == null) {
             return -1;
